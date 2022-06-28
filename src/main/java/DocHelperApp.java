6fb230bda3b105bbefca2ac64c2fd62adbabff54
ilakalasnikov <753476Kk>
2022-06-28 18:34:35 +0300

    Hibernate

--- src/main/java/DocHelperApp.java
+++ src/main/java/DocHelperApp.java
@@ -1,21 +1,52 @@
-import client.DocHelperClient;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import mapper.DocHelperMapper;
-import okhttp3.OkHttpClient;
-import service.DocHelperService;
+import model.dto.mkb_dto.MkbDto;
+import model.entity.MkbEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class DocHelperApp {
+    @SneakyThrows
     public static void main(String[] args) {
-        DocHelperService service = DocHelperService.builder()
-                .docHelperClient(DocHelperClient.builder()
-                        .okHttpClient(new OkHttpClient())
-                        .objectMapper(new ObjectMapper())
-                        .build())
-                .docHelperMapper(new DocHelperMapper())
+        ObjectMapper mapper = new ObjectMapper();
+        DocHelperMapper docHelperMapper = new DocHelperMapper();
+        Path path = Paths.get("MKB_codes");
+        File file = new File("src/main/resources/MKB_codes");
+
+        MkbDto mkbDto = mapper.readValue(file, MkbDto.class);
+        List<MkbEntity> mkbEntityList = docHelperMapper.mkbDtoToEntity(mkbDto);
+
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
                 .build();
-        service.checkClinRecAvailability("L93");
+
+
+
+        try (SessionFactory sessionFactory = new MetadataSources(registry)
+                .buildMetadata()
+                .buildSessionFactory();
+                Session session = sessionFactory.openSession()
+        ) {
+            Transaction transaction = session.beginTransaction();
+
+            for (int i = 0; i < mkbEntityList.size(); i++) {
+                MkbEntity entityToSave = new MkbEntity();
+                entityToSave.setCode(mkbEntityList.get(i).getCode());
+                entityToSave.setDisease(mkbEntityList.get(i).getDisease());
+                session.persist(entityToSave);
+            }
+            transaction.commit();
+        }
     }
 }
 
--- src/main/java/model/entity/MkbEntity.java
+++ src/main/java/model/entity/MkbEntity.java
@@ -1,13 +1,38 @@
 package model.entity;
 
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.experimental.FieldDefaults;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.hibernate.Hibernate;
 
-@Data
-@FieldDefaults(level = AccessLevel.PRIVATE)
+import java.util.Objects;
+
+@Entity
+@Table(name = "testMkb")
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class MkbEntity {
-    String disease;
+    @Id
     String code;
-    boolean clinRecAvailability = false;
+    @Column(length = 400)
+    String disease;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        MkbEntity mkbEntity = (MkbEntity) o;
+        return code != null && Objects.equals(code, mkbEntity.code);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
